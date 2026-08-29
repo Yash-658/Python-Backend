d5afe62ba05b6eb4e8426217d8601e7a832e789a
Your Name <136227872+Yash-658@users.noreply.github.com>
2026-08-29 11:25:55 +0530

    did database integration~

---
+++ database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+
+DATABASE_URL = "postgresql+psycopg://postgres:7974420106#yash@localhost:5432/coding_platform"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind = engine)
+
+class Base(DeclarativeBase):        # Eventually our coding platform will have many models, and they'll all inherit from Base.
+    pass
+
+class ProblemDB(Base):                # it inherits from Base, SQLAlchemy recognizes it as a database model.
+    __tablename__ = "problems"
+
+    id: Mapped[int] = mapped_column(primary_key=True)   # an int PK, so SQLAlchemy uses DB auto-increment by default
+    title: Mapped[str]                                  # Mapped[str] -> This is mapped to a DB column and its Python type is str
+    difficulty: Mapped[str]
+
+Base.metadata.create_all(engine)    # "Using this db engine, create all the tables described by my models if they don't already exist."
+
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel                      # FastAPI uses Pydantic heavily for validating and structuring incoming data.
+from database import SessionLocal, ProblemDB
+
+
+class Problem(BaseModel):     # data validation model (response model)
+    id: int
+    title: str
+    difficulty: str
+
+class createProblem(BaseModel):     # data validation model (request model)
+    title: str      
+    difficulty: str
+
 
 app = FastAPI()
 
@@ -7,52 +19,39 @@ app = FastAPI()
 async def root():
     return {'message':'hello doll'}
 
-problems = [
-    {
-        "id": 1,
-        "title": "Two Sum",
-        "difficulty": "Easy"
-    },
-    {
-        "id": 2,
-        "title": "Binary Tree Maximum Path Sum",
-        "difficulty": "Hard"
-    }
-]
-
-class Problem(BaseModel):     # data validation model
-    id: int
-    title: str
-    difficulty: str
-
-class createProblem(BaseModel):     # data validation model
-    title: str      
-    difficulty: str
 
 @app.get("/problems", response_model = list[Problem])    #FastAPI/Pydantic will turn the dictionaries into the declared response shape.
 async def get_problems():
+    db = SessionLocal()
+    problems = db.query(Problem).all()
+    db.close()
     return problems
 
 
 @app.get("/problems/{problem_id}", response_model = Problem)
 async def specific_prblm(problem_id:int):   #FastAPI will automatically return a validation error instead of your function receiving "abc" as a valid ID.
-    for problem in problems:
-        if(problem["id"] == problem_id):
-            return problem
+    db = SessionLocal()
+    this_problem = db.query(ProblemDB).filter(ProblemDB.id == problem_id).first()
+    db.close()
+    if this_problem:
+        return this_problem
 
-    raise HTTPException(
-        status_code=404,
-        detail = f"problem id {problem_id} not found!"
-    )
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail = f"problem id {problem_id} not found!"
+        )
 
 
 @app.post("/problems", response_model = Problem, status_code=status.HTTP_201_CREATED)    #Now FastAPI knows: For a POST /problems, expect a JSON body matching Problem.
 async def create_problem(problem:createProblem):
-    new_problem = {
-        "id":len(problems)+1,
-        "title":problem.title,
-        "difficulty":problem.difficulty
-    }
+    new_problem = ProblemDB(
+        title = problem.title,
+        difficulty = problem.difficulty
+    )
 
-    problems.append(new_problem)
+    db = SessionLocal()
+    db.add(new_problem)
+    db.commit()
+    db.close()
     return new_problem
