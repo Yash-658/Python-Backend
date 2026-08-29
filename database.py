ba0d9f8640547372033de64d48a8d2bf514362f0
Your Name <136227872+Yash-658@users.noreply.github.com>
2026-08-29 13:17:31 +0530

    added dependency injection for sessions

--- database.py
+++ database.py
@@ -4,12 +4,21 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 DATABASE_URL = "postgresql+psycopg://postgres:7974420106#yash@localhost:5432/coding_platform"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind = engine)
+SessionLocal = sessionmaker(bind = engine)    # creating session factory, now all sessions will be made using this~
+
+def get_db():                       # used for injecting dependencies, so instead of creating a new session everytime manually, we will pass this~
+    db = SessionLocal() 
+
+    try:
+        yield db                    # yield pauses the function and gives the database session to whoever requested it.
+
+    finally:
+        db.close()
 
 class Base(DeclarativeBase):        # Eventually our coding platform will have many models, and they'll all inherit from Base.
     pass
 
-class ProblemDB(Base):                # it inherits from Base, SQLAlchemy recognizes it as a database model.
+class ProblemDB(Base):              # it inherits from Base, SQLAlchemy recognizes it as a database model.
     __tablename__ = "problems"
 
     id: Mapped[int] = mapped_column(primary_key=True)   # an int PK, so SQLAlchemy uses DB auto-increment by default
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel                      # FastAPI uses Pydantic heavily for validating and structuring incoming data.
-from database import SessionLocal, ProblemDB
-
+from database import ProblemDB, get_db
+from sqlalchemy.orm import Session
 
 class Problem(BaseModel):     # data validation model (response model)
     id: int
@@ -16,42 +16,48 @@ class ProblemCreate(BaseModel):     # data validation model (request model)
 app = FastAPI()
 
 @app.get("/")
+
 async def root():
     return {'message':'hello doll'}
 
 
 @app.get("/problems", response_model = list[Problem])    #FastAPI/Pydantic will turn the dictionaries into the declared response shape.
-async def get_problems():
-    db = SessionLocal()
+
+async def get_problems(db: Session = Depends(get_db)):
     problems = db.query(ProblemDB).all()
-    db.close()
+
     return problems
 
 
 @app.get("/problems/{problem_id}", response_model = Problem)
-async def specific_prblm(problem_id:int):   #FastAPI will automatically return a validation error instead of your function receiving "abc" as a valid ID.
-    db = SessionLocal()
+
+async def specific_prblm(problem_id:int, db: Session = Depends(get_db)):   #FastAPI will automatically return a validation error instead of your function receiving "abc" as a valid ID.
     this_problem = db.query(ProblemDB).filter(ProblemDB.id == problem_id).first()
-    db.close()
+
     if this_problem:
         return this_problem
 
-    else:
-        raise HTTPException(
-            status_code=404,
-            detail = f"problem id {problem_id} not found!"
-        )
+    raise HTTPException(
+        status_code=404,
+        detail = f"problem id {problem_id} not found!"
+    )
 
 
 @app.post("/problems", response_model = Problem, status_code=status.HTTP_201_CREATED)    #Now FastAPI knows: For a POST /problems, expect a JSON body matching Problem.
-async def create_problem(problem:ProblemCreate):
+
+async def create_problem(problem:ProblemCreate, db: Session = Depends(get_db)):
     new_problem = ProblemDB(
         title = problem.title,
         difficulty = problem.difficulty
     )
 
-    db = SessionLocal()
-    db.add(new_problem)
-    db.commit()
-    db.close()
+    try:
+        db.add(new_problem)
+        db.commit()
+        db.refresh(new_problem)
+    
+    except Exception:
+        db.rollback()
+        raise 
+
     return new_problem   # SQLAlchemy has populated the generated id on your new_problem ORM object.
