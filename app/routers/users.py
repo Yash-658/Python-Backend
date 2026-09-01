9c914add01b71e36b31cf9cec8ac1e6d95c1c15a
Your Name <136227872+Yash-658@users.noreply.github.com>
2026-09-02 00:00:40 +0530

    added the login endpoints with handling both authenication failures, gotta deal with successfull login now~

--- app/routers/users.py
+++ app/routers/users.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import IntegrityError
 
 from sqlalchemy.orm import Session
-from app.schemas.users import UserCreate, UserResponse
+from app.schemas.users import UserCreate, UserResponse, UserLogin
 
 from app.database import get_db
 from app.models.userDB import UserDB
 
-from app.utils.security import hash_password
+from app.utils.security import hash_password, verify_password
 
 router = APIRouter(prefix = "/users", tags=["Users"])
 
@@ -37,6 +37,26 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     
     return new_user
 
+
+@router.post("/login")
+async def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    db_user = (db.query(UserDB).filter(UserDB.username == user.username).first())
+    
+    if db_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,       # We don't want: 404 User not found, because that reveals that the username doesn't exist. 
+            detail="Incorrect username or password!"        # Instead, we deliberately give the same message we'll use for an incorrect password, This prevents an attacker from easily discovering which usernames exist.
+        )
+
+    if not verify_password(user.password, db_user.password_hash):   # we don't simply hash (user.password) and check equality because password hashing uses a random salt, so hashing the same password again produces a different hash, verify_password() is specifically designed to handle this correctly.
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,       
+            detail="Incorrect username or password!"        
+        )
+        
 
 @router.get("", response_model=list[UserResponse])
 async def get_users(db: Session = Depends(get_db)):
--- app/schemas/users.py
+++ app/schemas/users.py
@@ -5,6 +5,11 @@ class UserCreate(BaseModel):
     email: str
     password: str
     
+    
+class UserLogin(BaseModel):
+    username: str
+    password: str
+    
 class UserResponse(BaseModel):
     id: int
     username: str
